8747d6b42f79f76e2b7673a2ef1e69e3d3ee7150
ArturSpirenkov <arturspirenkov@gmail.com>
2023-08-12 20:56:22 +0300

    app init board

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import BoardComponent from './components/BoardComponent.vue';
+import { Board } from './models/Board';
+import { ref, onMounted}from 'vue';
+
+const board = ref<Board>('')
+
+const restart = () => {
+  const newBoard = new Board()
+  newBoard.initCells()
+  newBoard.addFigures()
+  board.value = newBoard 
+}
+onMounted(() => {
+  restart()
+})
+</script>
+
+<template>
+  <div class="app">
+    <BoardComponent :board="board"/>
+  </div>
+</template>
+
+<style lang="scss">
+body{
+  margin: 0;
+  padding: 0;
+}
+.app {
+  width: 100vw;
+  height: 100vh;
+  background-color: antiquewhite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
