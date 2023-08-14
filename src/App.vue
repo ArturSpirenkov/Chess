78aef700edd51c846f04c3b901f2772e916f2221
ArturSpirenkov <arturspirenkov@gmail.com>
2023-08-14 22:57:37 +0300

    add players and current player, creatre method swapPlayer, add LostFigures in template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <script setup lang="ts">
-import BoardComponent from './components/BoardComponent.vue';
-import { Board } from './models/Board';
-import { ref, onMounted}from 'vue';
+import BoardComponent from './components/BoardComponent.vue'
+import { Board } from './models/Board'
+import { Player } from './models/Player'
+import { Colors } from './models/Colors'
+import { ref, onMounted}from 'vue'
+import LostFigures from './components/LostFigures.vue'
+import type { Figure } from './models/figures/figure'
 
 const board = ref<Board | null>(new Board())
+const whitePlayer = ref(new Player(Colors.WHITE))
+const blackPlayer = ref(new Player(Colors.BLACK))
+const currentPlayer = ref<Player | null>(null)
 
 const restart = () => {
   const newBoard = new Board()
   newBoard.initCells()
   newBoard.addFigures()
+  currentPlayer.value = whitePlayer.value
   board.value = newBoard 
 }
-onMounted(() => {
-  restart()
-})
 
 const refreshBoard = (e: Board) => {
   board.value = e
 }
+
+const swapPlayer = (): void => {
+  currentPlayer.value = currentPlayer.value?.color === Colors.WHITE ? blackPlayer.value : whitePlayer.value 
+}
+
+onMounted(() => {
+  restart()
+})
 </script>
 
 <template>
   <div class="app">
     <BoardComponent
-      :board="board"
-      @update-board="refreshBoard"/>
+      :board="(board as Board)"
+      @update-board="refreshBoard"
+      :currentPlayer="currentPlayer"
+      :swapPlayer="swapPlayer"
+    />
+    <div>
+      <LostFigures 
+        :title="Colors.BLACK"
+        :figures="(board!.lostBlackFigures as Figure[])"
+      />
+      <LostFigures 
+        :title="Colors.WHITE"
+        :figures="(board!.lostWhiteFigures as Figure[])"
+      />
+    </div>
   </div>
 </template>
 
