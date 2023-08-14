425412bc513c04a33f95776dd0e1e6d81a9147cb
ArturSpirenkov <arturspirenkov@gmail.com>
2023-08-14 22:48:08 +0300

    moving to another cell, check enemy, add lost figure

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -21,6 +21,13 @@ export class Cell {
     this.id = Math.random()
   }
 
+  isEnemy(target: Cell): boolean {
+    if (target.figure) {
+      return this.figure?.color !== target.figure.color;
+    }
+    return false;
+  }
+
   isEmpty(): boolean {
     return this.figure === null;
   }
@@ -68,5 +75,27 @@ export class Cell {
         return false;
     }
     return true;
+  }
+
+  setFigure(figure: Figure) {
+    this.figure = figure;
+    this.figure.cell = this;
+  }
+
+  addLostFigure(figure: Figure) {
+    figure.color === Colors.BLACK
+      ? this.board.lostBlackFigures.push(figure)
+      : this.board.lostWhiteFigures.push(figure)
+  }
+
+  moveFigure(target: Cell) {
+    if(this.figure && this.figure?.canMove(target)) {
+      this.figure.moveFigure(target)
+      if (target.figure) {
+        this.addLostFigure(target.figure);
+      }
+      target.setFigure(this.figure);
+      this.figure = null;
+    }
   }
 }
