d6f5c6d1f9b2bf1bc7550869c75b776ecd6cd7ea
ArturSpirenkov <arturspirenkov@gmail.com>
2023-08-29 23:40:55 +0300

    converting seconds to minutes

--- src/components/PlayerBoard.vue
+++ src/components/PlayerBoard.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import { Colors } from '@/models/Colors';
 import type { Player } from '@/models/Player';
-import { ref, watch, computed } from 'vue';
+import { watch, computed } from 'vue';
 import usePlayersTimer from '../hooks/usePlayersTimer';
+import { Colors } from '@/models/Colors';
 
-interface TimerProps {
+interface PlayerBoardProps {
   currentPlayer: Player | null
   restart: () => void
 }
 
-const props = defineProps<TimerProps>()
+const props = defineProps<PlayerBoardProps>()
 const player = computed((): Player | null => props.currentPlayer)
-const { whiteTime, blackTime, startTimer, restartTimer } = usePlayersTimer(player)
+const { startTimer, restartTimer, whiteTime, blackTime } = usePlayersTimer(player)
 
 watch(() => props.currentPlayer, () => {
   startTimer()
@@ -25,12 +25,75 @@ const handleRestart = () => {
 
 <template>
   <div>
-    <div>
-      <button @click="handleRestart">Restart game</button>
+    <div class="player-board">
+      <div :class="['player-board__black-player', 'black-player', { 'black-player_selected': currentPlayer?.color === Colors.BLACK }]">
+        <span class="black-player__time">{{ `${blackTime.min} : ${blackTime.sec}`}}</span>
+      </div>
+      <div class="restart-time" @click="handleRestart">
+        <button  class="restart-time__btn">Restart game</button>
+      </div>
+      <div :class="['player-board__white-player', 'white-player', {'white-player_selected': currentPlayer?.color === Colors.WHITE}]">
+        <span class="white-player__time">{{ `${whiteTime.min} : ${whiteTime.sec}`}}</span>
+      </div>
     </div>
-    <h2>Черные - {{ blackTime }}</h2>
-    <h2>Белые - {{ whiteTime }}</h2>
   </div>
 </template>
 <style lang="scss">
+@import '../assets/styles/mixin.scss';
+
+.player-board {
+  color: azure;
+  width: 70%;
+  height: 650px;
+  display: flex;
+  flex-direction: column;
+  max-width: 250px;
+  position: relative;
+  font-size: 30px;
+
+  &__white-player {
+    @include player;
+    border-radius: 0 0 10px 10px;
+  }
+  &__black-player {
+    @include player;
+    border-radius: 10px 10px 0 0;
+  }
+}
+
+.restart-time {
+  width: 70px;
+  height: 70px;
+  position: absolute;
+  background-image: linear-gradient(to right top, #004763, #004e6d, #005576, #005c80, #01638a);
+  top: calc(50% - 35px);
+  left: calc(50% - 35px);
+  rotate: 45deg;
+  border-radius: 7px;
+  box-shadow: 3px 3px 6px #7e8c93;
+  cursor: pointer;
+  &__btn {
+    background: none;
+    border: none;
+    height: 100%;
+    rotate: -45deg;
+    color: aliceblue;
+    font-size: 16px;
+    font-weight: 500;
+  }
+}
+.white-player {
+  &__time {
+  }
+  &_selected {
+    @include player-selected 
+  }
+}
+.black-player {
+  &__time {
+  }
+  &_selected {
+    @include player-selected 
+  }
+}
 </style>
--- src/hooks/usePlayersTimer.ts
+++ src/hooks/usePlayersTimer.ts
@@ -1,22 +1,32 @@
 import { Colors } from '@/models/Colors';
 import type { Player } from '@/models/Player';
-import { ref, type ComputedRef } from 'vue';
+import { ref, type ComputedRef, computed, watch } from 'vue';
 
 
 export default function usePlayersTimer(currentPlayer: ComputedRef<Player | null> , time = 300) {
-  const blackTime  = ref(time)
-  const whiteTime = ref(time)
+  const blackSec  = ref(time)
+  const whiteSec = ref(time)
   const timer = ref<null | ReturnType<typeof setInterval>>(null)
-  
+
+  const whiteTime = computed(() => ({
+    sec: String( whiteSec.value  % 60).padStart(2, '0'),
+    min: Math.floor(whiteSec.value / 60)
+  }))
+
+  const blackTime = computed(() => ({
+    sec: String( blackSec.value  % 60).padStart(2, '0'),
+    min: Math.floor(blackSec .value / 60)
+  }))
+
   const decrementBlackTimer = () => {
-    blackTime.value = blackTime.value - 1
+    blackSec.value = blackSec.value - 1
   }
   const decrementWhiteTimer = () => {
-    whiteTime.value = whiteTime.value - 1
+    whiteSec.value = whiteSec.value - 1
   }
   const restartTimer = () => {
-    blackTime.value = time
-    whiteTime.value = time
+    blackSec.value = time
+    whiteSec.value = time
   }
   const startTimer = () => {
     if (currentPlayer.value === null) {
@@ -30,9 +40,9 @@ export default function usePlayersTimer(currentPlayer: ComputedRef<Player | null
   }
  
   return {
-    whiteTime,
-    blackTime,
     startTimer,
-    restartTimer
+    restartTimer,
+    whiteTime,
+    blackTime
   }
 }
